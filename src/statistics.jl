f2a4a8093a5c04f1ee5d17a2949c9a33fe174b57
victinyGitHub <lordvicthedank@gmail.com>
2022-10-20 15:59:08 +0100

    what da dawg doin added whatioc which trials up to n specified periods and has an adjustable expectedvalue param. it returns the period with ioc closest to the expectedv

--- src/statistics.jl
+++ src/statistics.jl
@@ -24,3 +24,18 @@ function pioc(vtoken::Vector{Int64}, n::Int64, W::CSpace) ::Float64
 
     return avgIOC / n
 end
+
+
+function whatioc(expectedv::Float64, vtoken::Vector{Int64}, W::CSpace, trynum::Int64) ::Int64
+    #try up to j periodicities, compare with expected value, return value of n which gives closest ioc to expectedvalue of ioc 
+    bestioc = abs(b1000000-expectedv) # difference between bestioc and expected v
+    bestp = 1 #int of the most likely periodicity
+    for i in 1:trynum
+        # compare pioc of i with bestioc and see which one is closer to the expected ioc
+        ipioc = abs(expectedv-pioc(vtoken,i,W))
+        if ipioc < bestioc
+            bestioc=i #ill try implement something which lists the second third etc likeliest periodicity with difference values later icba rn
+        end
+    end
+    return bestioc
+end
