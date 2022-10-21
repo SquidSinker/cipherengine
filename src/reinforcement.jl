abd639f7a980eae6f564ae919262db61583c3465
rxwop <robiwop@gmail.com>
2022-10-21 13:18:42 +0100

    normal approximation implemented

--- src/reinforcement.jl
+++ src/reinforcement.jl
@@ -2,27 +2,35 @@ include("charspace.jl")
 include("substitution.jl")
 
 
-function binomial_pd(X, N, p)
-    return binomial(N, X) * p^X * (1-p)^(N-X)
+
+function normal_approx_pd(X, N, p)
+    mu = N * p
+    var = 2 * mu * (1 - p) # 2 carried through to reduce operations
+
+    return exp(- (X + 0.5 - N * p)^2 / var) / sqrt(pi * var)
 end
 
 function normalise(a::Vector)
     return a / sum(a)
 end
 
+
+
+
+
 # Limiting function taking (-inf, inf) -> (-p_old, p_new)
 function update_delta(delta_fitness, p_old, p_new; rate = 1)
-    mu = (p_new + p_old) / 2
-    diff = (p_new - p_old) / 2
+    mu = (p_new + p_old) # division by 2 carried out at the end
+    diff = (p_new - p_old) # division by 2 carried out at the end
 
     C = atanh(diff / mu)
 
-    return mu * tanh(delta_fitness * rate - C) + diff
+    return ( mu * tanh(delta_fitness * rate - C) + diff ) / 2
 end
 
 
 # Updates relevant PosProbMat entries based on delta_fitness
-function update_probs!(PosProbMat::Matrix, tokenA::Int, tokenB::Int, posA::Int, posB::Int, delta_fitness::Float64; reinforce_rate = 1)
+function update_PosProbMat!(PosProbMat::Matrix, tokenA::Int, tokenB::Int, posA::Int, posB::Int, delta_fitness::Float64; reinforce_rate = 1)
     dA = update_delta(delta_fitness, PosProbMat[tokenA, posA], PosProbMat[tokenA, posB]; reinforce_rate)
     dB = update_delta(delta_fitness, PosProbMat[tokenB, posB], PosProbMat[tokenB, posA]; reinforce_rate)
 
@@ -34,24 +42,38 @@ function update_probs!(PosProbMat::Matrix, tokenA::Int, tokenB::Int, posA::Int, 
 end
 
 
-function newPosProbMat(vect::Vector{Int}, W::CSpace; known_freq::Vector = nothing)
+
+
+
+function new_PosProbMat(vect::Vector{Int}, W::CSpace)
     L = length(vect)
     n = length(W.tokenisation)
 
-    if known_freq != nothing # if expected freq given, init PosProbMat as binomial guesses
-        Tallies = [count(x -> isequal(x, i), vect) for i in 1:n]
+    # uniform weighting
+    PosProbMat = ones((n, n)) / n
 
-        PosProbMat = hcat([normalise.(binomial_pd.(i, L, known_freq)) for i in Tallies]...)
-    else # else do uniform weighting
-        PosProbMat = ones((n, n)) / n
-    end
+    return PosProbMat
+end
+
+function new_PosProbMat(vect::Vector{Int}, W::CSpace, known_freq::Vector)
+    L = length(vect)
+    n = length(W.tokenisation)
+
+    # init PosProbMat as binomial guesses
+    Tallies = [count(x -> isequal(x, i), vect) for i in 1:n]
+
+    PosProbMat = hcat([normalise.(normal_approx_pd.(i, L, known_freq)) for i in Tallies]...)
 
     return PosProbMat
 end
 
 
 
-function predict_Sub(PosProbMat::Matrix) # THIS DOES NOT WORK IF Pij maxes on the same j for different i (repeats in Substitution)
+
+
+
+
+function predict_substitution(PosProbMat::Matrix) # THIS DOES NOT WORK IF Pij maxes on the same j for different i (repeats in Substitution)
     return Substitution( argmax.(eachcol(PosProbMat)) )
 end
 
@@ -59,9 +81,9 @@ using StatsBase # for sample()
 
 
 function linear_reinforcement(vect::Vector{Int}, W::CSpace, generations::Int, ChoiceWeights::Function, fitness::Function; known_freq::Vector = nothing, reinforce_rate = 1)
-    P = newPosProbMat(vect, W; known_freq)
+    P = new_PosProbMat(vect, W, known_freq)
 
-    S = predict_Sub(P)
+    S = predict_substitution(P)
     prev_fitness = fitness(invert(S)(vect))
 
     indices = Tuple.(keys(PosProbMat))
@@ -82,7 +104,7 @@ function linear_reinforcement(vect::Vector{Int}, W::CSpace, generations::Int, Ch
 
         dF = new_fitness - old_fitness
 
-        update_probs!(P, a, b, m, n, dF; reinforce_rate)
+        update_PosProbMat!(P, a, b, m, n, dF; reinforce_rate)
 
         old_fitness = new_fitness
     end
