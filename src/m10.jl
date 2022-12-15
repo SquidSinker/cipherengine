b713c24f096895e7be8b724feabb75e652cec537
rxwop <robiwop@gmail.com>
2022-12-15 19:20:03 +0000

    shift

--- src/m10.jl
+++ src/m10.jl
@@ -8,12 +8,44 @@ T = Txt("IHRHG FTVEE EENNP OAIAO LARAE IEETI NNBKL SPREE ETNRM RAHSE NEUNI IUMND
 tokenise!(T)
 
 
-block_n = 2
-block_l = Int(3250 / block_n)
+mutable struct Strange <: AbstractCipher
+    shifts::Vector{Int}
+    col_length::Int
+end
 
-txts = [T[1 + i*block_l:block_l + i*block_l] for i in 0:block_n-1]
+function shifter(v::Vector{Int}, shifts::Vector{Int})
+    v = reshape(v, (length(shifts), :))
 
-data = [substructure_sigma(Unshaper(n)(txts[1]), n) for n in 1:10]
+    for (i,j) in enumerate(shifts)
+        v[i, :] = circshift(v[i, :], j)
+    end
 
-using Plots
-bar(data)
+    return vec(v)
+end
+
+function apply(S::Strange, vect::Vector{Int}; safety_checks::Txt)
+    new_tokens = copy(vect)
+
+    L = length(vect)
+    block_l = length(S.shifts) * S.col_length
+
+    if L % block_l != 0
+        error("idk just fix")
+    end
+
+    blocks = [vect[1 + b - block_l:b] for b in block_l:block_l:lastindex(vect)]
+
+    new_tokens = vcat([shifter(i, S.shifts) for i in blocks]...)
+
+    return new_tokens
+end
+
+
+cipher = Strange([2,2,1,5,4], 5)
+
+a = 5
+b = 5
+
+brute_a = product([collect(1:b) for _ in 1:a]...)
+
+optimise(x -> x(T), vec([Strange(collect(A), b) for A in brute_a]), quadgramlog)
