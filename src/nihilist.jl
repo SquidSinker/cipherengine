ae092731519ff5d3ebb43047d86bb87e8ce8c2a2
rxwop <robiwop@gmail.com>
2022-12-25 18:57:57 +0000

    dont rember

--- src/nihilist.jl
+++ src/nihilist.jl
@@ -5,23 +5,18 @@ mutable struct NihilistTransposition <: AbstractCipher
     row_perm::Vector{Int}
     col_perm::Vector{Int}
 
-    rows_first::Bool
+    read_dim::Int
 
     inverted::Bool
 
-    function NihilistTransposition(row_perm::Vector{Int}, col_perm::Vector{Int}, rows_first::Bool = true)
-        new(row_perm, col_perm, rows_first, false)
+    function NihilistTransposition(row_perm::Vector{Int}, col_perm::Vector{Int}, read_dim::Int = 1)
+        new(row_perm, col_perm, read_dim, false)
     end
 end
 
 invert!(N::NihilistTransposition) = switch_invert_tag!(N)
 
 function apply(N::NihilistTransposition, v::Vector{Int}; safety_checks::Txt)
-    if N.rows_first
-        order = "rows"
-    else
-        order = "columns"
-    end
 
     vect = copy(v)
 
@@ -35,31 +30,30 @@ function apply(N::NihilistTransposition, v::Vector{Int}; safety_checks::Txt)
     blocks = [vect[1 + b - block_l:b] for b in block_l:block_l:lastindex(vect)]
 
     if N.inverted
-        return vcat([nihilist_trans_dec(i, N.row_perm, N.col_perm, order) for i in blocks]...)
+        return vcat([nihilist_trans_dec(i, N.row_perm, N.col_perm, N.read_dim) for i in blocks]...)
 
     else
-        return vcat([nihilist_trans_enc(i, N.row_perm, N.col_perm, order) for i in blocks]...)
+        return vcat([nihilist_trans_enc(i, N.row_perm, N.col_perm, N.read_dim) for i in blocks]...)
 
     end
 
 end
 
 
-function nihilist_trans_enc(plaintext::Vector, permrow, permcolumn, order="rows")
+function nihilist_trans_enc(plaintext::Vector, permrow, permcolumn, read_dim)
 
     len1 = length(permcolumn)
     len2 = length(permrow)
 
     plainmatrix = reshape(plaintext, (len1, len2))
-    if order == "rows"
-        plainmatrix = plainmatrix[:, permrow]
-        plainmatrix = plainmatrix[permcolumn, :]
-    elseif order == "columns"
-        plainmatrix = plainmatrix[permcolumn, :]
-        plainmatrix = plainmatrix[:, permrow]
-    else
-        error("argument order must be rows or columns")
+
+    plainmatrix = plainmatrix[:, permrow]
+    plainmatrix = plainmatrix[permcolumn, :]
+
+    if read_dim == 2
+        plainmatrix = permutedims(plainmatrix)
     end
+
     return vec(plainmatrix)
 end
 
@@ -71,14 +65,13 @@ function nihilist_trans_dec(ciphertext::Vector, permrow, permcolumn, order="rows
     invpermrow = [findfirst(==(i), permrow) for i in 1:len2]
     invpermcolumn = [findfirst(==(i), permcolumn) for i in 1:len1]
     ciphematrix = reshape(ciphertext, (len1, len2))
-    if order == "columns"
-        ciphematrix = ciphematrix[:, invpermrow]
-        ciphematrix = ciphematrix[invpermcolumn, :]
-    elseif order == "rows"
-        ciphematrix = ciphematrix[invpermcolumn, :]
-        ciphematrix = ciphematrix[:, invpermrow]
-    else
-        error("argument order must be rows or columns")
+
+    if read_dim == 2
+        ciphematrix = permutedims(ciphematrix)
     end
+
+    ciphematrix = ciphematrix[invpermcolumn, :]
+    ciphematrix = ciphematrix[:, invpermrow]
+
     return vec(ciphematrix)
 end
